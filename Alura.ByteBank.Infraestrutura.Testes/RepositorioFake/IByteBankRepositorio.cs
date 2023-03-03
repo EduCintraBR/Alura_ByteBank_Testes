c5820b41758b42c800b1e2ddb63806cbc4b30cb9
Eduardo Cintra <educin15@gmail.com>
2023-03-03 11:11:53 -0300

    Criação dos repositorios fakes ByteBankRepositorio e sua interface

---
+++ Alura.ByteBank.Infraestrutura.Testes/RepositorioFake/IByteBankRepositorio.cs
@@ -0,0 +1,15 @@
+﻿using Alura.ByteBank.Dominio.Entidades;
+using System.Collections.Generic;
+
+namespace Alura.ByteBank.Infraestrutura.Testes.RepositorioFake
+{
+    public interface IByteBankRepositorio
+    {
+        public List<Cliente> BuscarClientes();
+        public List<Agencia> BuscarAgencias();
+        public List<ContaCorrente> BuscarContasCorrentes();
+        public bool AdicionarConta(ContaCorrente conta);
+        public bool AdicionarAgencia(Agencia agencia);
+        public bool AdicionarCliente(Cliente cliente);
+    }
+}
